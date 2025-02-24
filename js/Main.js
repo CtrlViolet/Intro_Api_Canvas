245f67e77fc377c8803bff285bf8b1b46e8d13ed
CtrlViolet <mikyvit@hotmail.com>
2025-02-24 10:45:44 -0600

    Configurado Boostrap 5 V.3

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,17 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+// Obtener el elemento canvas del DOM por su ID
+let canvas = document.getElementById("canvas");
 
-ctx.fillStyle = "green";
-ctx.fillRect(10, 10, 100, 100);
+// Obtener el contexto de dibujo 2D del canvas para poder dibujar en él
+let ctx = canvas.getContext("2d");
+
+// Establecer el color de relleno utilizando valores RGB
+// En este caso, el color será un tono de azul: rgb(0, 0, 255)
+ctx.fillStyle = "rgb(13, 150, 192)";
+
+
+// Dibujar un rectángulo relleno en el canvas
+// El primer valor (50) es la posición horizontal (X) del vértice superior izquierdo del rectángulo
+// El segundo valor (60) es la posición vertical (Y) del vértice superior izquierdo del rectángulo
+// El tercer valor (150) es el ancho del rectángulo
+// El cuarto valor (150) es la altura del rectángulo
+ctx.fillRect(50, 60, 150, 150);
